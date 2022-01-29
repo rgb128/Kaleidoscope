9915f8d7d2c20b643e09e7312f3c461b73b02297
Olexandr Pascal <futupas@gmail.com>
2022-01-29 17:09:16 +0200

    one child

--- js/app.js
+++ js/app.js
@@ -2,63 +2,44 @@
 
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
-let state = 0; // 0 - filling canvas, 1 - making kaleidoscope (no click), 2 - show kaleidoscope
+let state = -1; // -1 - waiting, 0 - filling canvas, 1 - making kaleidoscope (no click), 2 - show kaleidoscope
 
-const colors = ['white', 'red', 'blue'];
+const colors = ['white', 'red', 'blue', 'green', 'yellow', 'orange', 'magenta', 'purple'];
 
-// canvas.width = document.documentElement.clientWidth;
-// canvas.height = document.documentElement.clientHeight;
 canvas.width = document.documentElement.clientWidth * 2;
 canvas.height = document.documentElement.clientHeight * 2;
 fillAllCanvas(canvas, 'black');
 
+let interval;
+
 document.addEventListener('click', e => {
-    console.log(e);
+    if (state === -1) {
+        state = 0;
+        addRandomRect();
+        interval = setInterval(() => {
+            addRandomRect();
+        }, 314);
+    } else if (state === 0) {
+        state = 1;
+        const container = document.getElementById('container');
+        const child = container.firstElementChild;
+        child.style.backgroundImage = `url('${canvas.toDataURL()}')`;
+        clearInterval(interval);
+        canvas.style.transform = 'scale(.5)';
+        setTimeout(() => {
+            const childCount = randomInt(10, 10);
+            const angle = 360 / childCount;
+            const width = document.documentElement.clientWidth;
+            const height = document.documentElement.clientHeight;
+            const triangleHalfWidth = height * Math.tan(angle / 2 * Math.PI / 180);
+            const x1 = width / 2 - triangleHalfWidth;
+            const x2 = width / 2 + triangleHalfWidth;
+            child.style.clipPath = `polygon(50% 100%, ${x1}px 0px, ${x2}px 0px)`;
+            child.style.top = '-50%';
+            container.classList.remove('hidden-item');
+            canvas.classList.add('hidden-item');
+            //todo fix animations
+        }, 0);
+    }
 });
 
-function addRandomRect() {
-    const maxWidth = canvas.width / 2;
-    const maxHeight = canvas.height / 2;
-
-    let x, y, width, height;
-
-    const side = randomInt(1, 4);
-    if (side === 1) { // Top
-        y = 0;
-        height = map(Math.random(), 0, 1, 1, maxHeight);
-        const x1 = map(Math.random(), 0, 1, 0, maxWidth);
-        const x2 = map(Math.random(), 0, 1, 0, maxWidth);
-        width = Math.abs(x1 - x2);
-        x = Math.min(x1, x2);
-    } else if (side === 2) { // Bottom
-        height = map(Math.random(), 0, 1, 1, maxHeight);
-        y = maxHeight - height;
-        const x1 = map(Math.random(), 0, 1, 0, maxWidth);
-        const x2 = map(Math.random(), 0, 1, 0, maxWidth);
-        width = Math.abs(x1 - x2);
-        x = Math.min(x1, x2);
-    } else if (side === 3) { // Left
-        x = 0;
-        width = map(Math.random(), 0, 1, 1, maxWidth);
-        const y1 = map(Math.random(), 0, 1, 0, maxHeight);
-        const y2 = map(Math.random(), 0, 1, 0, maxHeight);
-        height = Math.abs(y1 - y2);
-        y = Math.min(y1, y2);
-    } else if (side === 4) { // Right
-        width = map(Math.random(), 0, 1, 1, maxWidth);
-        x = maxWidth - width;
-        const y1 = map(Math.random(), 0, 1, 0, maxHeight);
-        const y2 = map(Math.random(), 0, 1, 0, maxHeight);
-        height = Math.abs(y1 - y2);
-        y = Math.min(y1, y2);
-    } else { console.error('Unknown side, not [1..4]') }
-
-    let strokeColor, strokeWidth;
-    if (randomInt(1, 2) === 1) { // with stroke
-        strokeWidth = map(Math.random(), 0, 1, 1, Math.min(width, height) / 3);
-        strokeColor = colors[randomInt(0, colors.length - 1)];
-    }
-    const fillColor = colors[randomInt(0, colors.length - 1)];
-
-    addRect(canvas, x, y, width, height, strokeWidth, strokeColor, fillColor);
-}
--- js/fill-canvas.js
+++ js/fill-canvas.js
@@ -76,3 +76,50 @@ function addRect(canvas, x, y, width, height, strokeWidth, strokeColor, fillColo
     addRectInternal(canvas, x3, y3, width, height, strokeWidth, strokeColor, fillColor);
     addRectInternal(canvas, x4, y4, width, height, strokeWidth, strokeColor, fillColor);
 }
+
+function addRandomRect() {
+    const maxWidth = canvas.width / 2;
+    const maxHeight = canvas.height / 2;
+
+    let x, y, width, height;
+
+    const side = randomInt(1, 4);
+    if (side === 1) { // Top
+        y = 0;
+        height = map(Math.random(), 0, 1, 1, maxHeight);
+        const x1 = map(Math.random(), 0, 1, 0, maxWidth);
+        const x2 = map(Math.random(), 0, 1, 0, maxWidth);
+        width = Math.abs(x1 - x2);
+        x = Math.min(x1, x2);
+    } else if (side === 2) { // Bottom
+        height = map(Math.random(), 0, 1, 1, maxHeight);
+        y = maxHeight - height;
+        const x1 = map(Math.random(), 0, 1, 0, maxWidth);
+        const x2 = map(Math.random(), 0, 1, 0, maxWidth);
+        width = Math.abs(x1 - x2);
+        x = Math.min(x1, x2);
+    } else if (side === 3) { // Left
+        x = 0;
+        width = map(Math.random(), 0, 1, 1, maxWidth);
+        const y1 = map(Math.random(), 0, 1, 0, maxHeight);
+        const y2 = map(Math.random(), 0, 1, 0, maxHeight);
+        height = Math.abs(y1 - y2);
+        y = Math.min(y1, y2);
+    } else if (side === 4) { // Right
+        width = map(Math.random(), 0, 1, 1, maxWidth);
+        x = maxWidth - width;
+        const y1 = map(Math.random(), 0, 1, 0, maxHeight);
+        const y2 = map(Math.random(), 0, 1, 0, maxHeight);
+        height = Math.abs(y1 - y2);
+        y = Math.min(y1, y2);
+    } else { console.error('Unknown side, not [1..4]') }
+
+    let strokeColor, strokeWidth;
+    if (randomInt(1, 2) === 1) { // with stroke
+        strokeWidth = map(Math.random(), 0, 1, 1, Math.min(width, height) / 3);
+        strokeColor = colors[randomInt(0, colors.length - 1)];
+    }
+    const fillColor = colors[randomInt(0, colors.length - 1)];
+
+    addRect(canvas, x, y, width, height, strokeWidth, strokeColor, fillColor);
+}
